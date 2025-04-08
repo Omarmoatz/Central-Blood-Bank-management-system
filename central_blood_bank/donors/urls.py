fb342c08e67526069ffbce95a009db6add82b018
Omarmoatz <omar.raouf.002@gmail.com>
2025-04-08 22:48:42 +0200

    add DonationResultView

--- central_blood_bank/donors/urls.py
+++ central_blood_bank/donors/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from central_blood_bank.donors.api.views import BloodStockViewSet
 from central_blood_bank.donors.api.views import DonorViewSet
-from central_blood_bank.donors.views import DonorRegisterView
+from central_blood_bank.donors.views import DonationResultView, DonorRegisterView
 
 router = DefaultRouter()
 router.register("donors", DonorViewSet, basename="donor")
@@ -11,7 +11,8 @@ router.register("blood-stocks", BloodStockViewSet, basename="blood-stock")
 
 urlpatterns = [
     path('donor/register/', DonorRegisterView.as_view(), name='donor_register'),
-    
+    path('donor/result/', DonationResultView.as_view(), name='donation_result'),
+
     *router.urls
 ]
 app_name = "donors"
--- central_blood_bank/donors/views.py
+++ central_blood_bank/donors/views.py
@@ -1,4 +1,6 @@
 from django.views.generic.edit import FormView
+from django.views.generic import TemplateView
+
 from django.urls import reverse_lazy
 from .forms import DonorForm
 from .service.donation_service import DonationService
@@ -12,3 +14,11 @@ class DonorRegisterView(FormView):
         donor = form.save()
         self.request.session['donation_msg'] = DonationService().evaluate_and_store_donation(donor.id)
         return super().form_valid(form)
+    
+class DonationResultView(TemplateView):
+    template_name = 'donation_result.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = self.request.session.pop('donation_msg', 'No result available.')
+        return context
