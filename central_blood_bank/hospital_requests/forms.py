5f554798ab5bd04cf6df37b4fb7e6de98dd36649
Omarmoatz <omar.raouf.002@gmail.com>
2025-04-09 17:33:57 +0200

    add creating a HospitalRequestView

--- central_blood_bank/hospital_requests/forms.py
+++ central_blood_bank/hospital_requests/forms.py
@@ -6,4 +6,9 @@ from central_blood_bank.hospital_requests.models import HospitalRequest
 class HospitalRequestForm(forms.ModelForm):
     class Meta:
         model = HospitalRequest
-        fields = '__all__'
+        fields = (
+            "hospital_name",
+            "blood_type",
+            "city",
+            "urgency_level",
+        )
--- central_blood_bank/hospital_requests/views.py
+++ central_blood_bank/hospital_requests/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.views import generic
 from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
@@ -10,14 +11,16 @@ class HospitalRequestListView(generic.ListView):
     model = HospitalRequest
     template_name = 'hospital_requests/hospital_request_list.html'
     context_object_name = 'requests'
+    paginate_by = 20
 
 class HospitalRequestView(FormView):
-    template_name = 'hospital_request/hospital_request.html'
+    template_name = 'hospital_requests/hospital_request.html'
     form_class = HospitalRequestForm
-    success_url = reverse_lazy('hospital_request')
+    success_url = reverse_lazy('hospital_requests:hospital_request_list')
 
     def form_valid(self, form):
         form.save()
         if HospitalRequest.objects.filter(status=HospitalRequest.StatusChoices.PENDING).count() >= 10:
             RequestService().handle_hospital_request_queue()
+        messages.success(self.request, "successfully created your request we will inform you after finishing the request")
         return super().form_valid(form)
